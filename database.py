369c775ea7a1606ed5a5224ef241023b6f3a1cf9
Hieulangtu <trunghieunguyen1905mu@gmail.com>
2024-11-11 00:56:36 +0100

    second day

--- database.py
+++ database.py
@@ -1,2 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+
+# Tạo một engine - một đối tượng quản lý kết nối với cơ sở dữ liệu.
+engine=create_engine('postgresql://postgres:1905@localhost/pizza_delivery',
+    echo=True
+)
+
+
+try:
+    # Thử kết nối
+    with engine.connect() as connection:
+        print("connect successfully to database !")
+except Exception as e:
+    print("can not connect to database")
+    print(e)
+
+Base=declarative_base()
+
+Session=sessionmaker()
+
+
+# hàm create_engine từ SQLAlchemy, cho phép bạn tạo kết nối tới cơ sở dữ liệu
+# declarative_base là một lớp cơ sở (base class) dùng để định nghĩa các mô hình (models) dưới dạng các lớp Python
+# sessionmaker là một hàm giúp tạo session để thực hiện các thao tác với cơ sở dữ liệu.
---
+++ init_db.py
@@ -0,0 +1,10 @@
+from database import engine,Base
+from models import User,Order
+
+
+Base.metadata.create_all(bind=engine)
+
+# File này thường được sử dụng để khởi tạo cơ sở dữ liệu khi bạn thiết lập 
+# ứng dụng lần đầu hoặc khi bạn cần tạo lại cấu trúc bảng. 
+# Chạy file init_db.py sẽ đảm bảo rằng các bảng trong cơ sở dữ liệu được tạo 
+# đúng theo cấu trúc bạn định nghĩa trong các model.
